8d6e72bb6f70c67ea9cd9d17ed9153eb39f1be3c
Antoine Poussier <antoine.poussier@etu.univ-grenoble-alpes.fr>
2023-12-06 19:27:06 +0100

    ajout routeur et view vides et genericButton

--- IHM_The_Garden/src/router/index.js
+++ IHM_The_Garden/src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import GardenView from '../views/GardenView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +7,26 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: GardenView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/garden/:username',
+      name: 'visitGarden',
+      props: true,
+      component: () => import('../views/GardenView.vue')
+    },
+    {
+      path: '/chat/',
+      name: 'mainChat',
+      component: () => import('../views/ChatView.vue')
+    },
+    {
+      path: '/chat/:username',
+      name: 'chatWithUser',
+      props: true,
+      component: () => import('../views/ChatView.vue')
+    },
   ]
-})
+});
 
-export default router
+export default router;
